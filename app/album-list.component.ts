14395611e8c0c09e6949a2d41b878904cf31b60a
James Turello <jturello@yahoo.com>
2016-08-11 03:49:08 -0700

    move album list display code from AppComponent to child AlbumListComponent

--- app/album-list.component.ts
+++ app/album-list.component.ts
@@ -3,12 +3,32 @@ import { Album } from './album.model';
 
 @Component({
   selector: 'album-list',
+  inputs: ['albumList'],
   template: `
-  <h1>Album List, Baby...</h1>
+
+    <h3>Album List</h3>
+    <div class="row" *ngFor="#album of albums" (click)="onClick(album)">
+      <div class='col-sm-3'>
+        {{ album.title }}
+      </div>
+      <div class='col-sm-3'>
+        {{ album.artist }}
+      </div>
+      <div class='col-sm-3'>
+        {{ album.genre }}
+      </div>
+      <div class='col-sm-3'>
+        {{ album.price }}
+      </div>
+    </div>
   `
 
 })
 
 export class AlbumListComponent {
+  public albumList: Album[];
+  onClick(clickedAlbum: Album): void {
+    console.log(clickedAlbum);
+  }
 
 }
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,16 @@
 import { Component } from 'angular2/core';
 import { Album } from './album.model';
+import { AlbumListComponent} from './album-list.component';
 
 @Component({
   selector: 'my-app',
+    directives: [AlbumListComponent],
   template: `
-    <div class="container">
-      <h1>Angular Music Store</h1>
-      <h3>Album List</h3>
-      <div class="row" *ngFor="#album of albums" (click)="onClick(album)">
-        <div class='col-sm-3'>
-          {{ album.title }}
-        </div>
-        <div class='col-sm-3'>
-          {{ album.artist }}
-        </div>
-        <div class='col-sm-3'>
-          {{ album.genre }}
-        </div>
-        <div class='col-sm-3'>
-          {{ album.price }}
-        </div>
-      </div>
-    </div>
+  <div class="container">
+    <h1>Angular Music Store</h1>
     <hr>
+    <album-list></album-list>
+  </div>
   `
 })
 export class AppComponent{
@@ -35,9 +23,4 @@ export class AppComponent{
       new Album('Album Four', 'Lucia Popp', 'Classical', '7.05')
     ]
   }
-
-  onClick(clickedAlbum: Album): void {
-    console.log(clickedAlbum);
-  }
-
 }
